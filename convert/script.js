e3cbd423f2f3e831c127a512f9869c1184fa70a8
2013 MBP 4GB 128GB <2013mbp4gb128gb@2013s-MBP.home.local>
2026-07-09 07:40:36 -0400

    Revert "implement custom javascript dropdown component"
    This reverts commit e27261d233945dfd0c2dc390fa0c8bea9b0dc60c.

--- convert/script.js
+++ convert/script.js
@@ -585,10 +585,6 @@ function initOmniDropdowns() {
     selFrom.value = "US Dollars (USD)";
     selTo.value = "A Slice of Pizza";
     
-    // Initialize custom selects
-    setupCustomSelect(selFrom);
-    setupCustomSelect(selTo);
-    
     fetchLivePrices();
     
     // Add event listeners that reset time
@@ -596,96 +592,5 @@ function initOmniDropdowns() {
     document.getElementById('omniFrom').addEventListener('change', () => runOmniConversion(true));
     document.getElementById('omniTo').addEventListener('change', () => runOmniConversion(true));
 }
-
-function setupCustomSelect(selElmnt) {
-    /* For each element, create a new DIV that will act as the selected item: */
-    let a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    selElmnt.parentNode.appendChild(a);
-    
-    /* For each element, create a new DIV that will contain the option list: */
-    let b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    
-    for (let j = 0; j < selElmnt.length; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-        let opt = selElmnt.options[j];
-        let c = document.createElement("DIV");
-        
-        // Handle optgroups
-        if (opt.parentNode.tagName === "OPTGROUP") {
-            // Only add the label if this is the first option in the group
-            if (opt === opt.parentNode.firstElementChild) {
-                let labelDiv = document.createElement("DIV");
-                labelDiv.setAttribute("class", "optgroup-label");
-                labelDiv.innerHTML = opt.parentNode.label;
-                b.appendChild(labelDiv);
-            }
-        }
-        
-        c.innerHTML = opt.innerHTML;
-        c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            let s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            let h = this.parentNode.previousSibling;
-            for (let i = 0; i < s.length; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    let y = this.parentNode.getElementsByClassName("same-as-selected");
-                    for (let k = 0; k < y.length; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-            
-            // Trigger change event for OmniConversion
-            s.dispatchEvent(new Event('change'));
-        });
-        b.appendChild(c);
-    }
-    selElmnt.parentNode.appendChild(b);
-    
-    a.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
-}
-
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    let x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
-    }
-}
-
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
 
 window.addEventListener('DOMContentLoaded', initOmniDropdowns);
